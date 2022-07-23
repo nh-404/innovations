00c5369ef73f38aa48edde181d8f6f76b87e557a
Tasnimul Hasan <63117803+nh-404@users.noreply.github.com>
2022-07-23 23:23:59 +0600

    Update Day counter.py
    - version: 1.1
    
    *add Graphic user interface
    *add manual day counter from any year
    *fix some buds
    
    Signed-off-by: Tasnimul Hasan <63117803+nh-404@users.noreply.github.com>

--- Day counter.py
+++ Day counter.py
@@ -1,38 +1,105 @@
+# define module
+
 from tkinter import *
 from datetime import *
 import turtle
+                                              # Day counter project
+  
+                                              #it help to count girls period days
+                                              #it help to count any product purchasing dayg like as phone, sim card.... etc
+                                            
 
-#user input
+#manual Day count
 
-turtle.bgcolor("black")
+def manualCount():
 
-year = int(turtle.numinput("Enter previous year:  ","year"))
-months = int(turtle.numinput("Enter previous month:  ","Month"))
-day = int(turtle.numinput("Enter previous date:  ","Date"))
+    #user input
+    
+    turtle.bgcolor("black")
+
+    past_year1 = int(turtle.numinput("Enter previous year:  ","1st prvious year"))
+    past_months1 = int(turtle.numinput("Enter previous month:  ","1st prvious Month"))
+    past_dayy = int(turtle.numinput("Enter previous date:  ","1st prvious Date"))
+
+    past_year2 = int(turtle.numinput("Enter previous year:  ","2nd previuos year"))
+    past_months2 = int(turtle.numinput("Enter previous month:  ","2nd previous Month "))
+    past_day2 = int(turtle.numinput("Enter previous date:  ","2nd previous Date"))
+
+    # past days calculation process
+
+    pastDay = date(past_year1, past_months1, past_dayy)
+    pastDay2 = date(past_year2, past_months2, past_day2)
+
+    dayCount = pastDay - pastDay2
+
+    #print result
+
+    Input = 401
+
+    if Input == 401:
+
+            turtle.color('cyan')
+            Style = ('Courier', 18, 'bold')
+
+            turtle.write( dayCount,' days',font=Style, align='center')
+            turtle.done()
+
+            
+#count from present date
+
+def autoCount():
+
+    #user input
+
+    turtle.bgcolor("black")
+
+    previous_year = int(turtle.numinput("Enter previous year:  ","previous year"))
+    previous_months = int(turtle.numinput("Enter previous month:  ","pervious Month"))
+    previous_day = int(turtle.numinput("Enter previous date:  ","previous Date"))
+
+    # days calculation process
+
+    today = datetime.now()
+    previous_date = date(previous_year, previous_months, previous_day)
+    present_date = today.date()
+
+    #math driver code
+    Day_count = present_date - previous_date
+
+    #print result
+
+    turtle.color('cyan')
+    Style = ('Courier', 18, 'bold')
+
+    turtle.write( Day_count,' days',font=Style, align='center')
+    turtle.done()
 
 
-today = datetime.now()
+                                                                # Graphic Driver code
 
-# days calculation process
+                                    
+    #define pop up window scaling
 
-previous_date = date(year, months , day)
+window = Tk()
+window.title('Day counter' )
+window.geometry('430x300')
+window.config(background="#000000")
+ 
+# define lable text
 
-present_date = today.date()
+lable = Label(window,text="Please choice your desire method 😊",height=1, width=50, font=('Arial',12),bg="cyan")
+lable.pack()
 
-Day_count = present_date - previous_date
+#button
 
+autocount_button = Button(window,text='Day count from Today',height=1,width=24,font=('Arial',14),bg= 'white',command=autoCount)
+autocount_button.pack()
 
-#print result
+manualcount_button = Button(window,text="Day count from any year",height=1,width=24,font=('Arial',14),bg= 'white',command=manualCount)
+manualcount_button.pack()
 
-turtle.color('cyan')
-
-Style = ('Courier', 18, 'bold')
-
-turtle.write( Day_count,' days',font=Style, align='center')
-
-turtle.done()
+window.mainloop()
 
 
 #created by 
 # Tasnimul Hasan
-
