da54a06c5b55019c59367f7ed333c016149316a1
Tasnimul Hasan <63117803+nh-404@users.noreply.github.com>
2022-12-24 01:16:06 +0600

    Add files via upload
    Signed-off-by: Tasnimul Hasan <63117803+nh-404@users.noreply.github.com>

---
+++ Demo Login/main.py
@@ -0,0 +1,13 @@
+from kivymd.app import MDApp
+
+class login(MDApp):
+    def build(self):
+        return
+    def prss(self):
+        print('login sucecssful')
+
+    def frgt(self):
+        print('forget password is clicked')
+
+
+login().run()
